034d5087981364d88f49844879cc9f683fb0ea27
ERIC-NGUGI <ericmwenja@gmail.com>
2024-03-20 19:08:21 +0000

    calculator

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as calculator
+    a = 10
+    b = 5
+    """addition"""
+    print("{} + {} = {}".format(a, b, calculator.add(a,b)))
+    """subtraction"""
+    print("{} - {} = {}".format(a, b, calculator.sub(a, b)))
+    """multiplication"""
+    print("{} * {} = {}".format(a, b, calculator.mul(a ,b)))
+    """division"""
+    print("{} / {} = {}".format(a, b, calculator.div(a, b)))
+
+
